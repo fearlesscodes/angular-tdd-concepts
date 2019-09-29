0217b703cbf622161bce28e5635603dd6b172889
Anton Truong <antont86@gmail.com>
2019-09-29 18:00:11 +0200

    Adds same test with another way of fetching the customer service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import {CustomerService} from './customer.service';
@@ -7,7 +7,10 @@ import {of} from 'rxjs';
 
 describe('AppComponent', () => {
   let customerService: CustomerService;
+  let customerServiceTestBed: CustomerService;
   let stateResult;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,42 +26,50 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    customerServiceTestBed = TestBed.get(CustomerService);
+    customerService = fixture.debugElement.injector.get(CustomerService);
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('test');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
   });
 
   it('show title when function of customer service', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-
-    const app = fixture.debugElement.componentInstance;
-
-    customerService = fixture.debugElement.injector.get(CustomerService);
-
     stateResult = spyOn(customerService, 'getCustomer').and.returnValue(of(false));
+    app.ngOnInit();
+    fixture.detectChanges();
+    expect(app.state).toBe(false);
+  });
+
+  it('show title when function of customer service. Gets service by Testbed', () => {
+    stateResult = spyOn(customerServiceTestBed, 'getCustomer').and.returnValue(of(false));
+    app.ngOnInit();
     fixture.detectChanges();
     expect(app.state).toBe(false);
   });
 
   it('check if setCustomer has been passed the correct variable', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    customerService = fixture.debugElement.injector.get(CustomerService);
     spyOn(customerService, 'setCustomer');
     customerService.setCustomer('hallo');
     expect(customerService.setCustomer).toHaveBeenCalledWith('hallo');
+  });
+
+  it('check if setCustomer has been passed the correct variable. Gets service by Testbed', () => {
+    spyOn(customerServiceTestBed, 'setCustomer');
+    customerServiceTestBed.setCustomer('hallo');
+    expect(customerServiceTestBed.setCustomer).toHaveBeenCalledWith('hallo');
   });
 });
